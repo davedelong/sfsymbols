51e9cf47db7c41f07c8b48d7c0cb424431846eeb
Dave DeLong <davedelong@me.com>
2019-07-28 16:17:48 -0600

    Initial Commit

---
+++ sfsymbols/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sfsymbols
+//
+//  Created by Dave DeLong on 7/28/19.
+//  Copyright © 2019 Syzygy Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
