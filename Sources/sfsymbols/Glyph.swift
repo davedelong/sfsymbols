170932b5f5c18f968b7b6e726c43679952813a46
Dave DeLong <davedelong@me.com>
2019-10-19 10:28:25 -0600

    Restoring earlier parsing
    The format of the CSV changed between the first and second version of SF Symbols. This change builds off @kireev165's modifications, by choosing an appropriate parse algorithm based on the assumed format of the CSV. Fixes #14. Closes #15.

--- Sources/sfsymbols/Glyph.swift
+++ Sources/sfsymbols/Glyph.swift
@@ -44,13 +44,31 @@ struct Glyph {
     
     let identifierName: String
     let fullName: String
+    let baseName: String
+    let isFilled: Bool
+    let variant: String?
+    let isRTL: Bool
     
     let boundingBox: CGRect
     let allowsMirroring: Bool
+    let appleOnly: Bool
+    let keywords: Array<String>
     let restrictionNote: String?
     
     
     init?(size: Size, pieces: Array<String>, inFont font: CTFont) {
+        if pieces.count == 12 {
+            // format of CSV used by the second SF Symbols beta app (b13)
+            self.init(v2Pieces: pieces, size: size, inFont: font)
+        } else if pieces.count == 56 {
+            // format of CSV used by the first SF Symbols beta app (b12)
+            self.init(v1Pieces: pieces, size: size, inFont: font)
+        } else {
+            return nil
+        }
+    }
+    
+    private init?(v2Pieces pieces: Array<String>, size: Size, inFont font: CTFont) {
         guard pieces.count == 12 else { return nil }
         let name = "uni\(pieces[3]).\(size.rawValue)"
         var glyph = CTFontGetGlyphWithName(font, name as CFString)
@@ -87,6 +105,63 @@ struct Glyph {
             self.restrictionNote = nil
         } else {
             self.restrictionNote = pieces[5]
+        }
+        
+        self.baseName = ""
+        self.isFilled = pieces[10].contains(".fill")
+        self.variant = nil
+        self.isRTL = pieces[6] == "rtl"
+        
+        self.appleOnly = pieces[4] == "TRUE"
+        self.keywords = CSVFields(pieces[0])
+    }
+    
+    private init?(v1Pieces pieces: Array<String>, size: Size, inFont font: CTFont) {
+        guard pieces.count == 56 else { return nil }
+        let name = "uni\(pieces[1]).\(size.rawValue)"
+        var glyph = CTFontGetGlyphWithName(font, name as CFString)
+        if glyph == 0 { return nil }
+        
+        self.font = font
+        self.glyph = glyph
+        
+        var box = CGRect.zero
+        CTFontGetBoundingRectsForGlyphs(font, .default, &glyph, &box, 1)
+        let paddedBox: CGRect
+        let paddedBoxMultiplier: CGFloat = box.origin.y > 0 ? 2 : -2
+        paddedBox = CGRect(x: 0, y: 0, width: box.width + (2 * box.origin.x), height: box.height + (paddedBoxMultiplier * box.origin.y))
+        
+        self.boundingBox = paddedBox
+        self.originOffset = box.origin
+        
+        let path = CTFontCreatePathForGlyph(font, glyph, nil)
+        
+        let copy: CGPath?
+        if box.origin.y > 0 {
+          copy = path
+        } else {
+          var transform = CGAffineTransform(translationX: 0, y: -2 * originOffset.y)
+          copy = path?.copy(using: &transform)
+        }
+        self.cgPath = copy ?? CGPath(rect: CGRect(origin: .zero, size: paddedBox.size), transform: nil)
+        
+        self.fullName = pieces[12]
+        self.identifierName = "symbol" + fullName.components(separatedBy: ".").map { $0.capitalized }.joined()
+        self.baseName = pieces[13]
+        self.variant = pieces[15].isEmpty ? nil : pieces[15]
+        self.isFilled = pieces[16] == "fill"
+        self.isRTL = pieces[17] != ""
+        
+        let keywords = pieces[39].trimmingCharacters(in: CharacterSet(charactersIn: "\""))
+        self.keywords = CSVFields(keywords).map { $0.trimmingCharacters(in: .whitespaces) }
+        
+        self.appleOnly = Bool(pieces[8].lowercased()) ?? false
+        self.allowsMirroring = pieces[43] == "TRUE"
+        
+        if pieces[47].isEmpty == true {
+            self.restrictionNote = nil
+        } else {
+            self.restrictionNote = pieces[47]
         }
     }
     
